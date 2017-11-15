2add58dec82a02d1d67ce54c4f75a0ec0c209a08
Tommaso Leonardi <tom@itm6.xyz>
2017-11-15 17:53:42 +0000

    Missing comma

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='bedparse',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'
+          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6'
       ],
       packages=['bedparse'],
