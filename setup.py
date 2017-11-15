b6b29947d1494154466e61b7de77fa2538a3c2ac
Tommaso Leonardi <tom@tleo.io>
2017-11-15 14:38:47 +0000

    Corrected python versions

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ setup(name='bedparse',
       classifiers=[
           'Development Status :: 3 - Alpha',
            'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.1',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
